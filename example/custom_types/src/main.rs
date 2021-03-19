13a83dd87b4c00987b61165c7564acb83e9eb966
bruno <bruno.pereira@4effect.com.br>
2021-03-18 23:34:24 -0300

    struct exercise 1

--- example/custom_types/src/main.rs
+++ example/custom_types/src/main.rs
@@ -4,13 +4,6 @@ struct Person{
     _age:u8
 }
 
-//unit
-struct Unit;
-
-
-//tuple
-struct Pair(i32, f32);
-
 struct Point{
     x:f32,
     y:f32
@@ -21,15 +14,37 @@ struct Rectangle{
     bottom_right: Point
 }
 
+fn rect_area(rec: Rectangle) -> f32{
+    let result: f32 ;
+    result = rec.top_left.x * rec.bottom_right.y;
+    result
+}
+
+fn square(point: Point, y: f32) -> Rectangle{
+    let base = point.x;
+     let _area = Rectangle {
+        top_left: point,
+        bottom_right: Point {x:y , y: base}       
+    };
+    _area
+}
+
 fn main() {
     let _name = String::from("Bruno Pereira");
     let _age = 32;
     let _bruno = Person{_name, _age};
     println!("{:?}", _bruno);
+    
+    let _point: Point = Point{ x: 10.0, y: 5.0};
+    let Point { x: top_edge, y: left_edge } = _point;
+    let bottom_right = Point {x: 10.0, .._point};
 
-    let _point: Point = Point{ x: 100.3, y: 0.4};
-    println!("Points x{} and y{}", _point.x, _point.y);
+    let _rectangle = Rectangle {
+        top_left: Point {x: top_edge, y: left_edge},
+        bottom_right: bottom_right        
+    };
 
-    let bottom_right = Point {x: 0.5, .._point};
-    println!("Bottom Right x{} and y{}", bottom_right.x, bottom_right.y);
+    println!("area of a rectangle:{}", rect_area(_rectangle));
+    println!("area of a rectangle by square:{}", rect_area(square( Point {x: 10.0, y: 5.0}, 0.5)));
+
 }
