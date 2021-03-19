0d10e50b6d4d4a90f730d7255369663059e7d82a
bruno <bruno.pereira@4effect.com.br>
2021-03-19 15:09:51 -0300

    activity custom types, structs

--- example/custom_types/src/main.rs
+++ example/custom_types/src/main.rs
@@ -14,28 +14,27 @@ struct Rectangle{
     bottom_right: Point
 }
 
-fn rect_area(rec: Rectangle) -> f32{
-    let result: f32 ;
-    result = rec.top_left.x * rec.bottom_right.y;
+fn rect_area(rec: Rectangle) -> f32 {
+    let result = rec.top_left.x * rec.bottom_right.y;
     result
 }
 
 fn square(point: Point, y: f32) -> Rectangle{
-    let base = point.x;
      let _area = Rectangle {
-        top_left: point,
-        bottom_right: Point {x:y , y: base}       
+         bottom_right: Point {x: point.x , y: y}       ,
+         top_left: Point {x: point.x , y: y}
     };
     _area
 }
 
 fn main() {
+
     let _name = String::from("Bruno Pereira");
     let _age = 32;
     let _bruno = Person{_name, _age};
     println!("{:?}", _bruno);
     
-    let _point: Point = Point{ x: 10.0, y: 5.0};
+    let _point = Point{ x: 10.0, y: 5.0};
     let Point { x: top_edge, y: left_edge } = _point;
     let bottom_right = Point {x: 10.0, .._point};
 
@@ -45,6 +44,8 @@ fn main() {
     };
 
     println!("area of a rectangle:{}", rect_area(_rectangle));
-    println!("area of a rectangle by square:{}", rect_area(square( Point {x: 10.0, y: 5.0}, 0.5)));
+    let _rectangle = square( Point {x: 10.0, y: 10.0}, 0.5);
+    println!("area of a rectangle by square:{}", rect_area(_rectangle));
 
+    
 }
